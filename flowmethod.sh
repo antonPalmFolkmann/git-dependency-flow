ec197618a640c116c8f27875df769fe46c804acd
AlexanderSkySpot <ahj@vetisearch.dk>
2023-03-08 12:24:07 +0100

    Corrected script

--- flowmethod.sh
+++ flowmethod.sh
@@ -28,17 +28,17 @@ fi
 
 
 echo "Cloning necessary git repositories..."
-if [ ! -d "./target-repo/"]
+if [ ! -d "./target-repo/" ]
 then
     git clone -q https://github.com/zeeguu/api.git target-repo
 fi
 
-if [ ! -d "./source-repo/"]
+if [ ! -d "./source-repo/" ]
 then
     git clone -q https://github.com/github/vscode-codeql-starter.git source-repo
 fi
 
-if [ ! -d "./git-dependency-flow/"]
+if [ ! -d "./git-dependency-flow/" ]
 then
     git clone -q https://github.com/antonPalmFolkmann/git-dependency-flow.git
 fi
