ae2a5175e17d8b4fbbc04c32e287a14bacb6867a
AntonITU <anpf@itu.dk>
2023-03-15 11:40:18 +0100

    Restructuring

---
+++ scripts/clone_repos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd /root/Documents/FlowMethod
+
+echo "Cloning necessary git repositories..."
+if [ ! -d "./target-repo/" ]
+then
+    git clone -q https://github.com/zeeguu/api.git target-repo
+fi
+
+if [ ! -d "./source-repo/" ]
+then
+    git clone -q https://github.com/github/vscode-codeql-starter.git source-repo
+fi
+
+if [ ! -d "./git-dependency-flow/" ]
+then
+    git clone -q https://github.com/antonPalmFolkmann/git-dependency-flow.git
+fi
+echo "Cloned git repositories"
