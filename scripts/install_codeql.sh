da9b828976439c52c8986695b38c05d1610dc0d0
AntonITU <anpf@itu.dk>
2023-03-15 12:02:37 +0100

    Docker container running properly

--- scripts/install_codeql.sh
+++ scripts/install_codeql.sh
@@ -7,13 +7,13 @@ then
         
     wget https://github.com/github/codeql-cli-binaries/releases/download/v2.12.3/codeql-linux64.zip
 
-    mkdir -p ~/Documents/FlowMethod && cd "$_"
+    mkdir -p ~/FlowMethod && cd "$_"
 
     unzip -o ~/Downloads/codeql-linux64.zip -d .
 
-    echo -e "\n# CodeQL\nexport PATH=$PATH:$HOME/Documents/FlowMethod/codeql" >> ~/.bashrc
+    echo -e "\n# CodeQL\nexport PATH=$PATH:$HOME/FlowMethod/codeql" >> ~/.bashrc
     
     codeql resolve languages
 else
-    mkdir -p ~/Documents/FlowMethod && cd "$_"
+    mkdir -p ~/FlowMethod && cd "$_"
 fi
