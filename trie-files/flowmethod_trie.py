38a89691a7ac8dbd699122a3ee9b665c88e05a8e
AntonITU <anpf@itu.dk>
2023-03-28 17:45:31 +0200

    Trie working

---
+++ trie-files/flowmethod_trie.py
@@ -0,0 +1,113 @@
+# Create class TrieNode. Inputs are self and a string of the module name.
+# Set the module name as an attribute of the node.
+# Set the path to the file containing the module as an attribute of the node.
+# Set the children of the node as an attribute of the node.
+# Set the node as a leaf node as an attribute of the node.
+import csv
+from typing import List
+
+
+class TrieNode():
+    def __init__(self, module: str):
+        self.module = module
+        self.path = None
+        self.children = {}
+
+# Create insert method of TrieNode. Inputs are self and a list of module 
+# components as strings and a string of the path to the file containing the module.
+# If the first module component is not in the children of the current node,
+# create a new node for it and add it to the children of the current node, and give the rest of the module components
+# as inputs in the new node.
+# If the first module component is in the children of the current node, give the rest of the module components
+# as inputs in the child node.
+# If the list of module components is empty, set the current node to be a leaf node and set the path to the file containing the module.
+    def insert(self, module_components: List[str], path: str):
+        if module_components:
+            if module_components[0] not in self.children:
+                self.children[module_components[0]] = TrieNode(module_components[0])
+            self.children[module_components[0]].insert(module_components[1:], path)
+        else:
+            self.path = path
+
+# Create search method of TrieNode. Inputs are self and a list of module components as strings.
+# If the first module component is not in the children of the current node, return None.
+# If the first module component is in the children of the current node, give the rest of the module components
+# as inputs in the child node.
+# If the list of module components is empty, return the path as a string.
+    def search(self, module_components: List[str]):
+        if module_components:
+            if module_components[0] not in self.children:
+                return None
+            return self.children[module_components[0]].search(module_components[1:])
+        else:
+            if self.path == None:
+                if "__init__" in self.children:
+                    return self.children["__init__"].path
+                else:
+                    return None
+            return self.path
+        
+# Create method print to print the trie.
+    def print(self, level=0):
+        print("  " * level, self.module)
+        for child in self.children.values():
+            child.print(level + 1)
+
+# Create class Trie. Import CSV file with module names and paths to files containing the modules from file "modules.csv"
+# with the first column containing the module names and the second column containing the paths.
+
+class Trie():
+    def importModules(self):
+        with open("modules.csv", "r") as csv_file:
+            reader = csv.DictReader(csv_file)
+            for line in reader:
+                path = line["pathLocation"]
+                module = line["modulePath"]
+                module_components = module.split(".")
+                self.root.insert(module_components, path)
+    
+    def importFunctions(self):
+        with open("functions.csv", "r") as csv_file:
+            reader = csv.DictReader(csv_file)
+            for line in reader:
+                path = line["pathLocation"]
+                module = line["functionModule"]
+                module_components = module.split(".")
+                self.root.insert(module_components, path)
+
+    def importClasses(self):
+        with open("classes.csv", "r") as csv_file:
+            reader = csv.DictReader(csv_file)
+            for line in reader:
+                path = line["fileLocation"]
+                module = line["classModule"]
+                module_components = module.split(".")
+                self.root.insert(module_components, path)
+                del module_components[-2]
+                self.root.insert(module_components, path)
+
+    def __init__(self):
+        self.root = TrieNode("")
+        self.importModules()
+        self.importFunctions()
+        self.importClasses()
+
+
+# Run the Trie class and print the trie.
+if __name__ == "__main__":
+    trie = Trie()
+    edges = []
+    with open("edges.csv", "r") as csv_file:
+            reader = csv.DictReader(csv_file)
+            for line in reader:
+                path = line["path"]
+                module = line["modulePath"]
+                module_components = module.split(".")
+                foundPath = trie.root.search(module_components)
+                if foundPath == None:
+                    print(module_components)
+                    foundPath = "NONE"
+                edges.append(str(path) + " " + ".".join(module_components) + " " + str(foundPath))
+    with open("new_edges.csv", "w") as writer:
+        for edge in edges:
+            writer.write(edge + "\n")
---
+++ trie-files/infomap-dictionary.py
@@ -0,0 +1,46 @@
+import csv
+
+def read_csv_file():
+    with open("flowEdges.csv", 'r') as data:
+        dictionary = csv.DictReader(data)
+        index = 1
+        vertices = {}
+        edges = []
+
+        for row in dictionary:
+            path = row["importer"]
+            modulePath = row["imported"]
+
+            if path not in vertices:
+                vertices[path] = index
+                index += 1
+            
+
+            if modulePath not in vertices:
+                vertices[modulePath] = index
+                index += 1
+            
+            edges.append(str(vertices[path]) + " " + str(vertices[modulePath]))
+            
+        return (vertices, edges)
+
+
+def write_to_pajek_format(vertices, edges):
+    with open('pajek.txt', 'w') as writer:
+        writer.write("*Vertices " + str(len(vertices)) + "\n")
+        for key, value in vertices.items():
+            string = str(value) + " \"" + key + "\""
+            writer.write(string + "\n")
+
+        writer.write("*Edges " + str(len(edges)) + "\n")
+        for edge in edges:
+            writer.write(edge + "\n")
+
+
+def main():
+    (vertices, edges) = read_csv_file()
+    write_to_pajek_format(vertices, edges)
+
+
+if __name__ == "__main__":
+    main()
