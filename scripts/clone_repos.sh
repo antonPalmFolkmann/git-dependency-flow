a0c477ad730268efaa4c49ca5db6bef3b1366e1c
Anton Palm Folkmann <69316986+antonPalmFolkmann@users.noreply.github.com>
2023-04-03 16:04:14 +0200

    Feature/prompt git url (#5)
    * Added reading git url
    
    * Added changing branch

--- scripts/clone_repos.sh
+++ scripts/clone_repos.sh
@@ -3,10 +3,6 @@
 cd /root/FlowMethod
 
 echo "Cloning necessary git repositories..."
-if [ ! -d "./target-repo/" ]
-then
-    git clone -q https://github.com/zeeguu/api.git target-repo
-fi
 
 if [ ! -d "./source-repo/" ]
 then
@@ -17,4 +13,17 @@ if [ ! -d "./git-dependency-flow/" ]
 then
     git clone -q https://github.com/antonPalmFolkmann/git-dependency-flow.git
 fi
+
+cd git-dependency-flow
+git checkout feature/prompt-git-url
+cd ..
+
 echo "Cloned git repositories"
+
+read -p  "Input target GitHub repository URL: " target_repo_url
+
+if [ ! -d "./target-repo/" ]
+then
+    git clone -q $target_repo_url target-repo
+fi
+
